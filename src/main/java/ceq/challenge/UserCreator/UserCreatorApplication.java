7dc9ec0fb00b18023fde6d99e3604c1b333e81c2
Ezequiel Lagomarsino <ealagomarsino@teco.com.ar>
2020-11-13 21:03:17 -0300

    Started with Initializr

---
+++ src/main/java/ceq/challenge/UserCreator/UserCreatorApplication.java
@@ -0,0 +1,13 @@
+package ceq.challenge.UserCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ceq/challenge/UserCreator/UserCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ceq.challenge.UserCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
